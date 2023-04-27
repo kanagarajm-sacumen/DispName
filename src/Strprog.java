8d7b194204fb46d9aabeb007b8898a2ab83f043a
kanagarajm-sacumen <kanagaraj.m@sacumen.com>
2023-04-27 17:49:54 +0530

    commited data

---
+++ src/Strprog.java
@@ -0,0 +1,10 @@
+public class Strprog {
+    public Strprog() {
+        String[] string = {
+                "raj", "ram", "ravi"
+        };
+        for (String getString : string) {
+            System.out.println(getString);
+        }
+    }
+}
